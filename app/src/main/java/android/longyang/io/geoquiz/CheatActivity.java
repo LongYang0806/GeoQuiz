fc2681c5c45a9e0c9b76bfef467c1c4f7e024106
longyang <jasonyl0806@gmail.com>
2015-05-29 09:11:07 -0700

    Util chapter 5

--- app/src/main/java/android/longyang/io/geoquiz/CheatActivity.java
+++ app/src/main/java/android/longyang/io/geoquiz/CheatActivity.java
@@ -1,7 +1,16 @@
 package android.longyang.io.geoquiz;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by longyang on 5/29/15.
  */
-public class CheatActivity {
+public class CheatActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
 }
--- app/src/main/java/android/longyang/io/geoquiz/QuizActivity.java
+++ app/src/main/java/android/longyang/io/geoquiz/QuizActivity.java
@@ -2,18 +2,25 @@ package android.longyang.io.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     private Button mTrueButton;
     private Button mFalseButton;
     private TextView mQuestionTextView;
-    private Button mNextButton;
+    private Button mCheatButton;
+    private ImageButton mNextButton;
+    private ImageButton mPreviousButton;
 
     private TrueFalse[] mQuestionBanks = new TrueFalse[] {
             new TrueFalse(R.string.question_oceans, true),
@@ -28,7 +35,10 @@ public class QuizActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        mQuestionIndex = savedInstanceState != null ? savedInstanceState.getInt(KEY_INDEX, 0) : 0;
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -48,14 +58,76 @@ public class QuizActivity extends Activity {
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setText(mQuestionBanks[mQuestionIndex].getmQuestion());
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mQuestionTextView.setText(mQuestionBanks[++mQuestionIndex % mQuestionBanks.length].getmQuestion());
+            }
+        });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mCheatButton = (Button) findViewById(R.id.cheatButton);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mQuestionTextView.setText(mQuestionBanks[++mQuestionIndex % mQuestionBanks.length].getmQuestion());
             }
         });
+
+        mPreviousButton = (ImageButton) findViewById(R.id.previous_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                --mQuestionIndex;
+                mQuestionIndex = mQuestionIndex < 0 ? mQuestionBanks.length + mQuestionIndex : mQuestionIndex;
+                mQuestionTextView.setText(mQuestionBanks[mQuestionIndex % mQuestionBanks.length].getmQuestion());
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle bundle) {
+        super.onSaveInstanceState(bundle);
+        Log.d(TAG, "onSaveInstanceState");
+        bundle.putInt(KEY_INDEX, mQuestionIndex);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     @Override
